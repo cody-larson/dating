f476e847aeec1126bec751ee9e0b51af6d649716
Cody Larson <cllarson16@gmail.com>
2019-05-06 01:08:01 -0400

    validation functions added

---
+++ model/valid.php
@@ -0,0 +1,39 @@
+<?php
+
+function validName($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return ctype_alpha($data) ? true : false;
+}
+
+function validAge($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return (ctype_digit($data)) && ($data > 18) ? true : false;
+}
+
+function validPhone($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return preg_match("/^[0-9]{3}-[0-9]{3}-[0-9]{4}$/", $data) ? true : false;
+}
+
+function validIndoor($interests) {
+    $validIndoor = array('tv', 'movies', 'cooking', 'board games', 'puzzles', 'reading', 'playing cards', 'video games');
+    return !array_diff($interests, $validIndoor) ? true : false;
+}
+
+function validOutdoor($interests) {
+    $validOutdoor = array('hiking', 'biking', 'swimming', 'collecting', 'walking', 'climbing');
+    return !array_diff($interests, $validOutdoor) ? true : false;
+}
+
+function validEmail($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return !filter_var($data, FILTER_VALIDATE_EMAIL) ? false : true;
+}
